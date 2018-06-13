dc291caa2eaffe24cdee5b53975143bcd8e12fa5
Jérôme Cuq <jeromelist@gmail.com>
2018-06-13 15:37:28 +0200

    code changed for kibana 6.2.2 compatibility, bugs fixed (config values, filter set, ...), auto cancel of current filtering if no results

--- public/slider.js
+++ public/slider.js
@@ -1,25 +1,50 @@
-define(function (require) {
-  require('plugins/kibana-slider-plugin/deps/angularjs-slider/rzslider.css');
-  require('plugins/kibana-slider-plugin/deps/angularjs-slider/rzslider.min.js');
-  require('plugins/kibana-slider-plugin/slider.less');
-  require('plugins/kibana-slider-plugin/sliderController');
-  require('ui/registry/vis_types').register(SliderVisProvider);
+//import { CATEGORY } from 'ui/vis/vis_category';
+import { VisFactoryProvider } from 'ui/vis/vis_factory';
+//import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 
-  function SliderVisProvider(Private) {
-    var TemplateVisType = Private(require('ui/template_vis_type/template_vis_type'));
+// we need to load the css ourselves
+import './slider.less';
+// we also need to load the controller used by the template
+import './sliderController';
+import optionsTemplate from './sliderOptions.html';
+import template from './slider.html';
 
-    return new TemplateVisType({
-      name: 'slider',
-      title: 'Slider Widget',
-      icon: 'fa-sliders',
-      description: 'Useful for filtering with sliders in dashboards.',
-      template: require('plugins/kibana-slider-plugin/slider.html'),
-      params: {
-        editor: require('plugins/kibana-slider-plugin/sliderOptions.html')
+import '../node_modules/angularjs-slider/dist/rzslider.css';
+import '../node_modules/angularjs-slider/dist/rzslider.min.js';
+
+// register the provider with the visTypes registry
+VisTypesRegistryProvider.register(SliderVisProvider);
+
+function SliderVisProvider(Private) {
+  const VisFactory = Private(VisFactoryProvider);
+  //const Schemas = Private(VisSchemasProvider);
+
+  // return the visType object, which kibana will use to display and configure new
+  // Vis object of this type.
+  return VisFactory.createAngularVisualization({
+    name: 'slider',
+    title: 'Slider Widget',
+    icon: 'fa-sliders',
+    description: 'Useful for filtering with sliders in dashboards.',
+    template,
+    visConfig: {
+      defaults: {
+        field: null,
+        title: "no title",
+        minValue: 0,
+        maxValue: 500,
+        step: 1
       },
-      requiresSearch: true
-    });
-  }
+      template: template
+    },
+    editorConfig: {
+      optionsTemplate,
+    },
+    requiresSearch: true,
+    responseHandler: 'none'
+  });
+}
 
-  return SliderVisProvider;
-});
+// export the provider so that the visType can be required with Private()
+export default SliderVisProvider;
--- public/sliderController.js
+++ public/sliderController.js
@@ -1,125 +1,134 @@
-define(function (require) {
-  var module = require('ui/modules').get('kibana/kibana-slider-plugin', ['kibana', 'rzModule']);
-  module.controller('KbnSliderVisController', function ($scope, $rootScope, Private, $filter) {
-    var queryFilter = Private(require('ui/filter_bar/query_filter'));
-    var buildRangeFilter = require('ui/filter_manager/lib/range');
-    var IndexedArray = require('ui/indexed_array');
-    var angular = require('angular');
+//import _ from 'lodash';
+import { FilterBarQueryFilterProvider } from 'ui/filter_bar/query_filter';
+import { IndexedArray } from 'ui/indexed_array';
+import { buildRangeFilter } from 'ui/filter_manager/lib/range';
+//import { angular } from 'angular';
+import { uiModules } from 'ui/modules';
+// get the kibana/metric_vis module, and make sure that it requires the 'kibana' module if it
+// didn't already
+const module = uiModules.get('kibana/kibana-slider-plugin', ['kibana', 'rzModule']);
 
-    $rootScope.plugin = {
-      sliderPlugin: {}
-    };
-    $scope.config = {
-        filterType: 'range',
-        selectedField: null,
-        title: "",
-        icon: "",
-        slider: {
-          min: 50,
-          max: 200,
-          options: {
-            floor: 0,
-            ceil: 450,
-            step: 1,
-            precision: 2,
-            onEnd: function(sliderId, modelValue, highValue) {
-              if($scope.config.selectedField) {
-                $scope.filter({gte:modelValue, lte:highValue});
-              }
-            }
+
+module.controller('KbnSliderVisController', function ($scope, $element, $rootScope, Private, $filter) {
+  const queryFilter = Private(FilterBarQueryFilterProvider);
+
+  $rootScope.plugin = {
+    sliderPlugin: {}
+  };
+  $scope.config = {
+    filterType: 'range',
+    selectedField: $scope.vis.params.selectedField,
+    title: $scope.vis.params.title,
+    icon: '',
+    slider: {
+      min: $scope.vis.params.minValue,
+      max: $scope.vis.params.maxValue,
+      options: {
+        floor: $scope.vis.params.minValue,
+        ceil: $scope.vis.params.maxValue,
+        step: $scope.vis.params.step,
+        precision: 2,
+        onEnd: function (sliderId, modelValue, highValue) {
+          if ($scope.config.selectedField) {
+            $scope.filter({ gte: modelValue, lte: highValue });
           }
         }
-    };
+      }
+    }
+  };
 
-    $scope.oldValue = null;
+  // we want to be able to cancel the new filter in case it gives no results
+  var previousFilterRange = null;
 
-    $scope.filter = function(value) {
-      var oldFilter = $scope.findFilter($scope.oldValue);
-      if(oldFilter == null) {
-        var rangeFilter = buildRangeFilter({name: $scope.config.selectedField},
-                                            value,
-                                            $scope.vis.indexPattern);
-        queryFilter.addFilters(rangeFilter);
+  // where range = {gte: minValue, lte: maxValue}
+  $scope.filter = function (range) {
+    const currentFilter = $scope.findFilter();
+
+    if(currentFilter) {
+      previousFilterRange = currentFilter.meta.params;
+      queryFilter.removeFilter(currentFilter);
+    }
+    else
+      previousFilterRange = null;
+
+    const fieldName = $scope.config.selectedField;
+    if(fieldName && fieldName.length > 0 && range) {
+      const rangeFilter = buildRangeFilter({ name: fieldName },
+        range,
+        $scope.vis.indexPattern);
+      queryFilter.addFilters(rangeFilter);
+    }
+  };
+
+  $scope.findFilter = function () {
+    const filters = queryFilter.getFilters();
+    const fieldName = $scope.config.selectedField;
+    const matchingFilter = filters.find(function (f) {
+      return f.meta.key === fieldName;
+    });
+    return matchingFilter;
+  };
+
+  $scope.$watch('vis.params.field', function (field) {
+    if (field) {
+      $scope.config.selectedField = field.name;
+    }
+  });
+  $scope.$watch('vis.params.minValue', function (minValue) {
+    if (minValue) {
+      $scope.config.slider.options.floor = minValue;
+      $scope.config.slider.min = minValue;
+    }
+  });
+  $scope.$watch('vis.params.maxValue', function (maxValue) {
+    if (maxValue) {
+      $scope.config.slider.options.ceil = maxValue;
+      $scope.config.slider.max = maxValue;
+    }
+  });
+  $scope.$watch('vis.params.step', function (step) {
+    if (step) {
+      $scope.config.slider.options.step = step;
+    }
+  });
+  $scope.$watch('vis.params.title', function (title) {
+    $scope.config.title = title;
+  });
+  $scope.$watch('vis.params.icon', function (icon) {
+    $scope.config.icon = icon;
+  });
+
+  $scope.getIndexedNumberFields = function () {
+    let fields = $scope.vis.indexPattern.fields.raw;
+    const fieldTypes = ['number'];
+
+    if (fieldTypes) {
+      fields = $filter('fieldType')(fields, fieldTypes);
+      fields = $filter('filter')(fields, {});
+      fields = $filter('orderBy')(fields, ['type', 'name']);
+    }
+
+    return new IndexedArray({
+      index: ['name'],
+      initialSet: fields
+    });
+  };
+
+  $scope.$watch('esResponse', function (resp) {
+    if (resp.hits.total==0) {
+      // If the response doesn't show any value, we cancel current filter to prevent the slider from disappearing
+      if (previousFilterRange) {
+        $scope.config.slider.min = previousFilterRange.gte;
+        $scope.config.slider.max = previousFilterRange.lte;
       }
       else {
-        $scope.editingFilter = {
-          source: oldFilter, // old
-          type: $scope.config.filterType,
-          model: angular.copy(oldFilter), // new
-          alias: oldFilter.meta.alias
-        };
-        $scope.editingFilter.model.range[$scope.config.selectedField].gte = $scope.config.slider.min;
-        $scope.editingFilter.model.range[$scope.config.selectedField].lte = $scope.config.slider.max;
-        queryFilter.updateFilter($scope.editingFilter);
-      }
-      $scope.oldValue = value;
-    };
-
-    $scope.findFilter = function(oldValue) {
-      var foundFilter = null;
-
-      if(oldValue != null) {
-        var oldFilter = buildRangeFilter({name: $scope.config.selectedField},
-                                          oldValue,
-                                          $scope.vis.indexPattern);
-
-        var filters = queryFilter.getFilters();
-        for (var i = 0; i < filters.length; i++) {
-          if (_.isEqual(filters[i][$scope.config.filterType], oldFilter[$scope.config.filterType])) {
-            foundFilter = filters[i];
-            break;
-          }
-        }
+        $scope.config.slider.min = $scope.config.slider.options.floor;
+        $scope.config.slider.max = $scope.config.slider.options.ceil;
       }
 
-      return foundFilter;
-    };
-
-    $scope.$watch('vis.params.field', function (field) {
-      if(field) {
-        $scope.config.selectedField = field.name;
-      }
-    });
-    $scope.$watch('vis.params.minValue', function (minValue) {
-      if(minValue) {
-        $scope.config.slider.options.floor = minValue;
-        $scope.config.slider.min = minValue;
-      }
-    });
-    $scope.$watch('vis.params.maxValue', function (maxValue) {
-      if(maxValue) {
-        $scope.config.slider.options.ceil = maxValue;
-        $scope.config.slider.max = maxValue;
-      }
-    });
-    $scope.$watch('vis.params.step', function (step) {
-      if(step) {
-        $scope.config.slider.options.step = step;
-      }
-    });
-    $scope.$watch('vis.params.title', function (title) {
-      $scope.config.title = title;
-    });
-    $scope.$watch('vis.params.icon', function (icon) {
-      $scope.config.icon = icon;
-    });
-
-    $scope.getIndexedNumberFields = function() {
-      var fields = $scope.vis.indexPattern.fields.raw;
-      var fieldTypes = ["number"];
-
-      if (fieldTypes) {
-        fields = $filter('fieldType')(fields, fieldTypes);
-        fields = $filter('filter')(fields, { });
-        fields = $filter('orderBy')(fields, ['type', 'name']);
-      }
-
-      return new IndexedArray({
-        index: ['name'],
-        initialSet: fields
-      });
-    };
-
-    $rootScope.plugin.sliderPlugin.indexedFields = $scope.getIndexedNumberFields();
+      $scope.filter(previousFilterRange);
+    }
   });
+
+  $rootScope.plugin.sliderPlugin.indexedFields = $scope.getIndexedNumberFields();
 });
